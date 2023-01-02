1da3592407738ed3bf4af5a65e59694e9ea9f99b
Fernando Piacezzi <fernandopiacezzi@Fernandos-Mac-mini.local>
2023-01-02 10:47:55 -0300

    q2e

---
+++ scroll-on-screen/script.js
@@ -0,0 +1,39 @@
+const track = document.getElementById('image-track')
+
+window.onmousedown = e => {
+  track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmouseup = e => {
+  track.dataset.mouseDownAt = 0;
+  track.dataset.prevPercentage = track.dataset.percentage
+}
+
+window.onmousemove = e => {
+  if(track.dataset.mouseDownAt == '0') return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * -100;
+
+  let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+  nextPercentage = Math.min(nextPercentage, 0)
+  nextPercentage = Math.max(nextPercentage, -100)
+
+  
+
+  track.dataset.percentage = nextPercentage
+
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+
+  }, {duration:1200, fill: 'forwards'})
+
+  for(const image of track.getElementsByClassName('image')) {
+    image.animate({
+      objectPosition: `${nextPercentage + 100}% 50%`
+    }, {duration: 1200, fill: 'forwards'})
+  }
+}
